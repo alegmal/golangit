efb8043e890787ed9694620976302c0cef8d6954
Alegma <6178232+alegmal@users.noreply.github.com>
2024-11-06 00:33:28 +0200

    a flag for ls

--- ls/ls.go
+++ ls/ls.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
@@ -12,11 +13,21 @@ func checkForError(e error) {
 }
 
 func main() {
+	showHidden := flag.Bool("a", false, "show hidden files and directories")
+	flag.Parse()
+
+	defer fmt.Print("\n")
+
 	files, err := os.ReadDir(".")
 	checkForError(err)
 
+	if *showHidden {
+		fmt.Print(". .. ")
+	}
+
 	for _, file := range files {
-		// fmt.Println(file.Name(), file.IsDir())
-		fmt.Print(file.Name(), " ")
+		if *showHidden || file.Name()[0] != '.' {
+			fmt.Print(file.Name() + " ")
+		}
 	}
 }
