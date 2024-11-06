ec6293c0e9ac77df31e2931b468cdcc4439eb598
Alegma <6178232+alegmal@users.noreply.github.com>
2024-11-07 01:16:01 +0200

    begining of -l support for ls

--- ls/ls.go
+++ ls/ls.go
@@ -14,6 +14,7 @@ func checkForError(e error) {
 
 func main() {
 	showHidden := flag.Bool("a", false, "show hidden files and directories")
+	listLong := flag.Bool("l", false, "show more details")
 	flag.Parse()
 
 	defer fmt.Print("\n")
@@ -27,7 +28,15 @@ func main() {
 
 	for _, file := range files {
 		if *showHidden || file.Name()[0] != '.' {
-			fmt.Print(file.Name() + " ")
+			if *listLong {
+				info, err := os.Stat(file.Name())
+				checkForError(err)
+
+				fmt.Println(info.Mode(), " ", file.Name(), " ")
+			} else {
+				fmt.Print(file.Name(), " ")
+			}
 		}
 	}
+
 }
